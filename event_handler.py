182b65dea80d8ff3e300133f746c8cedcb2fa5ad
joh29 <joh29@uclive.ac.nz>
2020-10-11 14:33:06 +1300

    event listeners

--- event_handler.py
+++ event_handler.py
@@ -4,21 +4,32 @@ from engine.vector2 import Vector2
 
 
 class _Handler:
-    """ Should this be static?
-    Maybe even just a bunch of functions? """
+
+    last_events = None
+    listeners = None
+
+    def __init__(self):
+        self.events = []
+        self.listeners = {}
+
+        self.add_listener(pygame.VIDEORESIZE, lambda e: Display.resize(Vector2(e.w, e.h)))
+        self.add_listener(pygame.QUIT, lambda e: quit())
+
+    def add_listener(self, event_type, function):
+        """ Add a listener for the events.
+        The listener is to be a function that takes the pygame event associated with it. """
+        if event_type not in self.listeners:
+            self.listeners[event_type] = []
+        self.listeners[event_type].append(function)
 
     def run(self):
-        events = pygame.event.get()
-        for event in events:
-
-            # Quit button
-            if event.type == pygame.QUIT:
-                quit()
-
-            if event.type == pygame.VIDEORESIZE:
-                Display.resize(Vector2(event.w, event.h))
-
-        return events
+        """ Run the event handler.
+        Should the listeners be run? """
+        self.events = pygame.event.get()
+        for event in self.events:
+            if event.type in self.listeners:
+                for function in self.listeners[event.type]:
+                    function(event)
 
 
 EventHandler = _Handler()
